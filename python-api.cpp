78cdd7c2df3611c1555b4d136467b62b0327d9f2
Wei Dong <wdong@wdong.org>
2018-11-16 00:30:14 -0500

    remove useless templates

--- python-api.cpp
+++ python-api.cpp
@@ -20,10 +20,10 @@ namespace {
     using std::vector;
 
     class Voxelizer {
-        float z_min, z_max, z_factor;
-        float y_min, y_max, y_factor;
         float x_min, x_max, x_factor;
-        int nz, ny, nx;
+        float y_min, y_max, y_factor;
+        float z_min, z_max, z_factor;
+        int nx, ny, nz;
     public:
         Voxelizer (np::ndarray ranges,
                    np::ndarray shape) {
@@ -36,12 +36,12 @@ namespace {
                 CHECK(D == 3);
                 CHECK(W == 2);
                 float const *ptr = (float const *)ranges.get_data();
-                z_min = *ptr++;
-                z_max = *ptr++;
-                y_min = *ptr++;
-                y_max = *ptr++;
                 x_min = *ptr++;
                 x_max = *ptr++;
+                y_min = *ptr++;
+                y_max = *ptr++;
+                z_min = *ptr++;
+                z_max = *ptr++;
             }
             {
                 CHECK(shape.get_nd() == 1);
@@ -49,22 +49,22 @@ namespace {
                 int D = shape.shape(0);
                 CHECK(D == 3);
                 int const *ptr = (int const *)shape.get_data();
-                nz = *ptr++;
-                ny = *ptr++;
                 nx = *ptr++;
+                ny = *ptr++;
+                nz = *ptr++;
             }
 
-            float z_range = z_max - z_min;
-            float y_range = y_max - y_min;
             float x_range = x_max - x_min;
+            float y_range = y_max - y_min;
+            float z_range = z_max - z_min;
 
             // quantization method
             // [min, max], range   -->   [0, n-1]
             // q(x) = round(((x - min)/range) * (n-1))
             //      = round((x - min) * factor)
-            z_factor = (nz - 1) / z_range;
-            y_factor = (ny - 1) / y_range;
             x_factor = (nx - 1) / x_range;
+            y_factor = (ny - 1) / y_range;
+            z_factor = (nz - 1) / z_range;
         }
 
         static int quantize (float v, float min, float factor) {
@@ -75,43 +75,34 @@ namespace {
         py::tuple voxelize_points (py::list list, int T) { // np::ndarray points, int T) {
             int batch = py::len(list);
             CHECK(batch > 0);
-            int C;
+            int Cin;
             {   // checkout the first item in batch
                 np::ndarray points = py::extract<np::ndarray>(list[0]);
                 CHECK(points.get_nd() == 2);
                 CHECK(points.get_dtype() == np::dtype::get_builtin<float>());
-                C = points.shape(1);
+                Cin = points.shape(1);
+                CHECK(Cin == 4);
             }
-            //py::tuple ret;
+            int C = Cin + 3;
             int nv = nx * ny * nz;
-            // voxel points
-            np::ndarray P = np::zeros(py::make_tuple(batch, nv, T, C), np::dtype::get_builtin<float>());
-            int P_voxel_stride = T * C;
-            CHECK(nv * P_voxel_stride * int(sizeof(float)) == P.strides(0));
-            float *pp = (float *)P.get_data();
 
-            // voxel lengths (# points in voxel)
-            np::ndarray L = np::zeros(py::make_tuple(batch, nv, 1), np::dtype::get_builtin<int>());
-            CHECK(nv * int(sizeof(int)) == L.strides(0));
-            float *ll = (float *)L.get_data();
-
-            vector<vector<float const*>> voxels(nv);
-            for (int i = 0; i < batch; ++i) {//, pp += P_batch_stride, ll += L_batch_stride) {
+            vector<vector<float const*>> voxels(batch * nv);
+            for (int i = 0; i < batch; ++i) {
                 // foreach batch
                 np::ndarray points = py::extract<np::ndarray>(list[i]);
-                CHECK(points.shape(1) == C);
+                CHECK(points.shape(1) == Cin);
                 CHECK(points.get_nd() == 2);
                 CHECK(points.get_dtype() == np::dtype::get_builtin<float>());
                 int N = points.shape(0);
-                CHECK(points.strides(0) == C * int(sizeof(float)));
+                CHECK(points.strides(0) == Cin * int(sizeof(float)));
                 CHECK(points.strides(1) == int(sizeof(float)));
 
-                for (auto &v: voxels) v.clear();
-                // distribute points to voxels
-                for (int j = 0; j < N; ++j) {
-                    // one point
+                vector<unsigned> index(N);
+                for (unsigned j = 0; j < N; ++j) index[j] = j;
+                std::random_shuffle(index.begin(), index.end());
+
+                for (unsigned j: index) {
                     float const *ptr = (float const *)(points.get_data() + points.strides(0) * j);
-                    // TODO: check order of xyz
                     float x = ptr[0];
                     float y = ptr[1];
                     float z = ptr[2];
@@ -123,26 +114,95 @@ namespace {
                     int iz = quantize(z, z_min, z_factor);
                     if (iz < 0 || iz >= nz) continue;
 
-                    int cell = (iz * ny + iy) * nx + ix;
-                    voxels[cell].push_back(ptr);
-                }
-
-                // copy the voxels to output
-                for (auto &voxel: voxels) {
-                    if (voxel.size() > unsigned(T)) { 
-                        std::random_shuffle(voxel.begin(), voxel.end());
-                        voxel.resize(T);
+                    int cell = i * nv + (ix * ny + iy) * nz + iz;
+                    if (voxels[cell].size() < T) {
+                        voxels[cell].push_back(ptr);
                     }
-                    *ll++ = voxel.size();
-                    float *p = pp; // copy points within voxel
-                    for (float const *from: voxel) {
-                        std::copy(from, from + C, p);
-                        p += C;
-                    }
-                    pp += P_voxel_stride;
                 }
             }
-            return py::make_tuple(P, L);
+
+            int NZ = 0; // non-empty entries
+            for (auto const &v: voxels) {
+                if (v.size() > 0) ++NZ;
+            }
+            // voxel points
+            np::ndarray P = np::zeros(py::make_tuple(NZ, T, C), np::dtype::get_builtin<float>());
+            int P_voxel_stride = T * C;
+            CHECK(P_voxel_stride * int(sizeof(float)) == P.strides(0));
+            float *pp = (float *)P.get_data();
+
+            // voxel lengths (# points in voxel)
+            np::ndarray M = np::zeros(py::make_tuple(NZ, T, 1), np::dtype::get_builtin<float>());
+            int M_voxel_stride = T;
+            CHECK(T * int(sizeof(float)) == M.strides(0));
+            float *mm = (float *)M.get_data();
+
+            np::ndarray I = np::zeros(py::make_tuple(NZ), np::dtype::get_builtin<int32_t>());
+            int32_t *ii = (int32_t *)I.get_data();
+
+            for (unsigned j = 0; j < voxels.size(); ++j) {
+                auto const &voxel = voxels[j];
+                if (voxel.empty()) continue;
+                *ii++ = j;
+
+                float *p = pp; // copy points within voxel
+                float *m = mm;
+                float x = 0, y = 0, z = 0;
+                for (float const *from: voxel) {
+                    x += from[0];
+                    y += from[1];
+                    z += from[2];
+                }
+                float cnt = voxel.size();
+                if (cnt > 0) {
+                    x /= cnt;
+                    y /= cnt;
+                    z /= cnt;
+                }
+                for (float const *from: voxel) {
+                    p[0] = from[0];
+                    p[1] = from[1];
+                    p[2] = from[2];
+                    p[3] = from[3];
+                    p[4] = from[0]-x;
+                    p[5] = from[1]-y;
+                    p[6] = from[2]-z;
+                    p += C;
+                    *m++ = 1.0;
+                }
+                pp += P_voxel_stride;
+                mm += M_voxel_stride;
+            }
+            return py::make_tuple(P, M, I);
+        }
+
+        py::tuple make_dense (np::ndarray P, np::ndarray I) {
+            CHECK(P.get_nd() == 2);
+            CHECK(P.get_dtype() == np::dtype::get_builtin<float>());
+            int C = P.shape(1);
+            int nv = nx * ny * nz;
+            CHECK(I.get_nd() == 1);
+            CHECK(P.shape(0) == I.shape(0));
+            int N = P.shape(0);
+
+            // infer batch from last index in I
+            int batch = 0;
+            {
+                int32_t const *ii = (int32_t const *)(I.get_data() + (N-1) * I.strides(0));
+                batch = (ii[0] / nv) + 1;
+            }
+
+            np::ndarray V = np::zeros(py::make_tuple(batch, nx, ny, nz, C), np::dtype::get_builtin<float>());
+            CHECK(nv * C * int(sizeof(float)) == V.strides(0));
+            float *vv = (float *)V.get_data();
+
+            for (int i = 0; i < N; ++i) {
+                float const *pp = (float const *)(P.get_data() + i * P.strides(0));
+                int32_t const *ii = (int32_t const *)(I.get_data() + i * I.strides(0));
+                float *oo = vv + ii[0] * C;
+                std::copy(pp, pp + C, oo);
+            }
+            return py::make_tuple(V);
         }
 
         py::tuple voxelize_labels (py::list list, np::ndarray priors, int downsize) { //np::ndarray labels, int downsize) {
@@ -150,18 +210,18 @@ namespace {
             CHECK(nz % downsize == 0);
             CHECK(ny % downsize == 0);
             CHECK(nx % downsize == 0);
-            int lz = nz / downsize;
-            int ly = ny / downsize;
             int lx = nx / downsize;
+            int ly = ny / downsize;
+            //int lz = nz / downsize;
 
             CHECK(priors.get_nd() == 2);
-            int params = priors.shape(1);
+            int params = 4; //priors.shape(1);
             // anchors and masks
-            np::ndarray A = np::zeros(py::make_tuple(batch, lz, ly, lx, priors.shape(0)), np::dtype::get_builtin<float>());
-            np::ndarray AW = np::zeros(py::make_tuple(batch, lz, ly, lx, priors.shape(0)), np::dtype::get_builtin<float>());
+            np::ndarray A = np::zeros(py::make_tuple(batch, lx, ly, priors.shape(0)), np::dtype::get_builtin<float>());
+            np::ndarray AW = np::zeros(py::make_tuple(batch, lx, ly, priors.shape(0)), np::dtype::get_builtin<float>());
             // parameters and masks
-            np::ndarray P = np::zeros(py::make_tuple(batch, lz, ly, lx, priors.shape(0), params), np::dtype::get_builtin<float>());
-            np::ndarray PW = np::zeros(py::make_tuple(batch, lz, ly, lx, priors.shape(0)), np::dtype::get_builtin<float>());
+            np::ndarray P = np::zeros(py::make_tuple(batch, lx, ly, priors.shape(0) * params), np::dtype::get_builtin<float>());
+            np::ndarray PW = np::zeros(py::make_tuple(batch, lx, ly, priors.shape(0)), np::dtype::get_builtin<float>());
 
             /*
             int P_voxel_stride = T * C;
@@ -178,6 +238,7 @@ BOOST_PYTHON_MODULE(cpp)
     np::initialize();
     py::class_<Voxelizer>("Voxelizer", py::init<np::ndarray, np::ndarray>())
         .def("voxelize_points", &Voxelizer::voxelize_points)
+        .def("make_dense", &Voxelizer::make_dense)
         .def("voxelize_labels", &Voxelizer::voxelize_labels)
     ;
 }
--- train_voxelnet.py
+++ train_voxelnet.py
@@ -5,7 +5,7 @@ sys.path.append('build/lib.linux-x86_64-' + sys.version[:3])
 import random
 import tensorflow as tf
 import aardvark
-import rpn3d            # read aardvark/rpn3d.py for BasicRPN3D
+import rpn            # read aardvark/rpn.py for RPN
 from zoo import net3d   # read aardvark/zoo/net3d.py
 import cpp
 from kitti import *
@@ -22,60 +22,16 @@ DOWNSIZE = 2
 
 '''
 T = 32
-RANGES = [[-3, 1],
-          [-40, 40],
-          [0, 70.4]]
-INPUT_SHAPE = [16, 400, 352]
-
-class BasicVoxelNet (aardvark.Model, rpn3d.BasicRPN3D):
-    # This is the base class of VoxelNet
-    # this class sets up basic inference pipeline for Kitti-like
-    # point cloud data:
-    #   1. Input is a bunch of voxels, each voxel cointaining a variable
-    #      number of points.
-    #   2. A sub-network to extract features from each voxel
-    #   3. A standard 3D CNN for box regression (via BasicRPN3D)
-
-    # The actual implementation of rpn and vfe components are to be
-    # implemented by a subclass.
-
-    def __init__ (self):
-        aardvark.Model.__init__(self)
-        rpn3d.BasicRPN3D.__init__(self)
-        pass
-
-    def vfe (self, points, lengths):
-        # points:       N * T * C
-        # lengths:      N * 1
-        assert False
-
-    def build_graph (self):
-        self.is_training = tf.placeholder(tf.bool, name="is_training")
-
-        Z, Y, X = INPUT_SHAPE
-        V = Z * Y * X
-        # 'points' grouped by voxels, each group should have been pre-capped at T
-        #       with empty ones filled with 0s
-        # then length of each group is given by 'lengths'
-        self.points = tf.placeholder(tf.float32, name='points',
-                                shape=(FLAGS.batch, V, T, FLAGS.channels))
-        self.lengths = tf.placeholder(tf.float32, name='lengths',
-                                shape=(FLAGS.batch, V, 1))
-        flatten_points = tf.reshape(self.points, (FLAGS.batch * V, T, FLAGS.channels))
-        flatten_lengths = tf.reshape(self.lengths, (FLAGS.batch * V, 1))
-        # we send a batch of voxels into the vfe_net
-        # the shape of the 3D grid shouldn't concern vfe_net
-        net = self.vfe(flatten_points, flatten_lengths)
-        net = tf.reshape(net, (FLAGS.batch, Z, Y, X, -1))
-
-        self.build_rpn(net, self.is_training, shape=(INPUT_SHAPE))
-        pass
+RANGES = [[0, 70.4],    # X
+          [-40, 40],    # Y
+          [-3, 1]]      # Z
+INPUT_SHAPE = [352, 400, 8]
 
 
 class Stream:
     # Streaming samples, mimic picpac API
-    def __init__ (self, path, priors, is_training):
-        self.vxl = cpp.Voxelizer(np.array(RANGES, dtype=np.float32), np.array(INPUT_SHAPE, dtype=np.int32))
+    def __init__ (self, path, vxl, priors, is_training):
+        self.vxl = vxl
         self.priors = priors
 
         samples = []
@@ -108,14 +64,14 @@ class Stream:
                     meta = lambda: None
                     setattr(meta, 'ids', np.zeros((1,)))
 
-                    assert sample.points.shape[1] == FLAGS.channels, 'channels should be %d.' % sample.points.shape[1]
+                    assert sample.points.shape[1] == 4, 'channels should be %d.' % sample.points.shape[1]
 
-                    points, lengths = self.vxl.voxelize_points([sample.points], T)
+                    points, mask, index = self.vxl.voxelize_points([sample.points], T)
 
                     boxes = sample.get_boxes_array(["Car"])
-                    anchors, anchors_weight, params, params_weight = self.vxl.voxelize_labels([boxes], self.priors, FLAGS.rpn_stride)
+                    anchors, anchors_weight, params, params_weight = self.vxl.voxelize_labels([boxes], np.array(self.priors, dtype=np.float32), FLAGS.rpn_stride)
 
-                    yield meta, points, lengths, anchors, anchors_weight, params, params_weight
+                    yield meta, points, mask, index, anchors, anchors_weight, params, params_weight
                 if not self.is_training:
                     break
         self.generator = generator()
@@ -128,35 +84,132 @@ class Stream:
         return next(self.generator)
 
 
-class VoxelNet (BasicVoxelNet):
+def conv2d (net, ch, kernel, strides, is_training):
+    net = tf.layers.conv2d(net, ch, kernel, strides, padding='same')
+    net = tf.layers.batch_normalization(net, training=is_training)
+    net = tf.nn.relu(net)
+    return net
+
+def deconv2d (net, ch, kernel, strides, is_training):
+    net = tf.layers.conv2d_transpose(net, ch, kernel, strides, padding='same')
+    net = tf.layers.batch_normalization(net, training=is_training)
+    net = tf.nn.relu(net)
+    return net
+
+def conv3d (net, ch, kernel, strides, is_training): 
+    net = tf.layers.conv3d(net, ch, kernel, strides, padding='same')
+    net = tf.layers.batch_normalization(net, training=is_training)
+    net = tf.nn.relu(net)
+    return net
+
+class VoxelNet (rpn.RPN):
+    # This is the base class of VoxelNet
+    # this class sets up basic inference pipeline for Kitti-like
+    # point cloud data:
+    #   1. Input is a bunch of voxels, each voxel cointaining a variable
+    #      number of points.
+    #   2. A sub-network to extract features from each voxel #   3. A standard 3D CNN for box regression (via BasicRPN3D)
+
+    # The actual implementation of rpn and vfe components are to be
+    # implemented by a subclass.
+
     def __init__ (self):
         super().__init__()
+        self.vxl = cpp.Voxelizer(np.array(RANGES, dtype=np.float32), np.array(INPUT_SHAPE, dtype=np.int32))
         pass
 
-    def vfe (self, net, lengths):
-        # net: N * T * C
-        # lengths: N * 1
-        net = tf.reduce_sum(net, axis=1) / (lengths + 0.00001)
+    def vfe (self, net, mask):
+        net = tf.expand_dims(net, axis=0)
+        # net: 1 * V * T * C
+        # lengths: 1 * V * T * 1
+        for ch in [32, 128]:
+            net = conv2d(net, ch//2, 1, 1, self.is_training)
+            # net: B * V * T * C'
+            net = net * mask
+            pool = tf.reduce_max(net, axis=2, keepdims=True)
+            # net: B * V * 1 * C'
+            pool = tf.tile(pool, [1, 1, T, 1])
+            net = tf.concat([net, pool], axis=3)
+
+        net = conv2d(net, 128, 1, 1, self.is_training)
+        net = net * mask
+        net = tf.reduce_max(net, axis=2)
+        net = tf.squeeze(net, axis=0)
         return net
 
-    def rpn_backbone (self, volume, is_training, stride):
-        assert stride == 1
-        net, st = net3d.unet(volume, is_training)
+    def middle (self, net):
+        # input is 352 x 400 x 10 x ?
+        #
+        for ch, str3 in [(64, 2), (64, 1), (64, 2)]:
+            net = conv3d(net, ch, 3, (1,1,str3), self.is_training)
         return net
 
-    def rpn_logits (self, net, is_training, channels):
-        return tf.layers.conv3d(net, channels, 3, strides=1, activation=None, padding='SAME')
+    def rpn_backbone (self, net):
+        is_training = self.is_training
+        net = conv2d(net, 128, 3, 2, is_training)
+        for _ in range(3):
+            net = conv2d(net, 128, 3, 1, is_training)
+        block1 = net
+        net = conv2d(net, 128, 3, 2, is_training)
+        for _ in range(5):
+            net = conv2d(net, 128, 3, 1, is_training)
+        block2 = net
+        net = conv2d(net, 256, 3, 2, is_training)
+        for _ in range(5):
+            net = conv2d(net, 256, 3, 1, is_training)
+        net = deconv2d(net, 256, 4, 4, is_training)
+        net2 = deconv2d(block2, 256, 2, 2, is_training)
+        net1 = deconv2d(block1, 256, 3, 1, is_training)
+        net = tf.concat([net, net1, net2], axis=3)
+        self.backbone = net
+        pass
 
-    def rpn_params (self, net, is_training, channels):
-        return tf.layers.conv3d(net, channels, 3, strides=1, activation=None, padding='SAME')
+    def rpn_logits (self, channels, strides):
+        assert strides == 2
+        return tf.layers.conv2d(self.backbone, channels, 1, strides=1, activation=None, padding='SAME')
+
+    def rpn_params (self, channels, strides):
+        return tf.layers.conv2d(self.backbone, channels, 1, strides=1, activation=None, padding='SAME')
+
+    def rpn_non_max_supression (self, boxes, index, prob):
+        nms_max = tf.constant(FLAGS.nms_max, dtype=tf.int32, name="nms_max")
+        nms_th = tf.constant(FLAGS.nms_th, dtype=tf.float32, name="nms_th")
+        return tf.image.non_max_suppression(rpn.shift_boxes(boxes, index), prob, nms_max, iou_threshold=nms_th)
+
+    def build_graph (self):
+        self.is_training = tf.placeholder(tf.bool, name="is_training")
+
+        X, Y, Z = INPUT_SHAPE
+        V = Z * Y * X
+        # 'points' grouped by voxels, each group should have been pre-capped at T
+        #       with empty ones filled with 0s
+        # then length of each group is given by 'lengths'
+        self.points = tf.placeholder(tf.float32, name='points',
+                                shape=(None, T, FLAGS.channels))
+        self.mask = tf.placeholder(tf.float32, name='mask',
+                                shape=(None, T, 1))
+        self.index = tf.placeholder(tf.int32, name='index',
+                                shape=(None,))
+        # we send a batch of voxels into the vfe_net
+        # the shape of the 3D grid shouldn't concern vfe_net
+        net = self.vfe(self.points, self.mask)
+        net, = tf.py_func(self.vxl.make_dense, [net, self.index], [tf.float32])
+        net = tf.reshape(net, (FLAGS.batch, X, Y, Z, 128))
+
+        net = self.middle(net)
+        net = tf.reshape(net, (FLAGS.batch, X, Y, -1))
+
+        self.build_rpn(net)
+        pass
 
     def create_stream (self, path, is_training):
-        return Stream(path, self.priors, is_training)
+        return Stream(path, self.vxl, self.priors, is_training)
 
     def feed_dict (self, record, is_training = True):
-        _, pts, l, a, aw, p, pw = record
+        _, pts, mask, index, a, aw, p, pw = record
         return {self.points: pts,
-                self.lengths: l,
+                self.mask: mask,
+                self.index: index,
                 self.gt_anchors: a,
                 self.gt_anchors_weight: aw,
                 self.gt_params: p,
@@ -164,9 +217,10 @@ class VoxelNet (BasicVoxelNet):
                 self.is_training: is_training}
     pass
 
+
 def main (_):
     #./train.py --classes 2 --db data/multi_train_trim.list  --val_db data/multi_val_trim.list --epoch_steps 20 --ckpt_epochs 1 --val_epochs 1000
-    FLAGS.channels = 4
+    FLAGS.channels = 7
     FLAGS.classes = None    # we shouldn't need this
     FLAGS.db = 'kitti_data/train.txt'
     FLAGS.val_db = 'kitti_data/val.txt'
@@ -174,7 +228,7 @@ def main (_):
     FLAGS.ckpt_epochs = 1
     FLAGS.val_epochs = 1000
     FLAGS.model = "vxlnet"
-    FLAGS.rpn_stride = 1
+    FLAGS.rpn_stride = 2
     model = VoxelNet()
     aardvark.train(model)
     pass
