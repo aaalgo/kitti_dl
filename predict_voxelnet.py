0217bd2bba19e755054313af94415097bbb8d4d1
Wei Dong <wdong@wdong.org>
2018-11-16 22:11:21 -0500

    update

--- predict_voxelnet.py
+++ predict_voxelnet.py
@@ -3,7 +3,8 @@ from train_voxelnet import *
 from gallery import Gallery
 
 flags = tf.app.flags
-flags.DEFINE_integer('max', 10, '')
+flags.DEFINE_integer('max', 20, '')
+flags.DEFINE_string('gallery', None, '')
 FLAGS = flags.FLAGS
 
 def main (_):
@@ -18,15 +19,17 @@ def main (_):
     is_val = True
     if is_val:
         db = FLAGS.val_db
+        columns = 2 # two columns to visualize groundtruth
     else:
         db = FLAGS.test_db
+        columns = 1
 
     with tf.Session(config=config) as sess:
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
         saver.restore(sess, FLAGS.model)
 
-        gal = Gallery('output')
+        gal = Gallery('output', cols=columns)
         C = 0
         with open(db, 'r') as f:
             for l in f:
@@ -39,12 +42,22 @@ def main (_):
                              model.mask: mask,
                              model.index: index}
 
-                if False:
-                    # test if probs and params generation in training is correct
-                    boxes = sample.get_boxes_array(["Car"])
-                    probs, _, params, _ = model.vxl.voxelize_labels([boxes], np.array(model.priors, dtype=np.float32), FLAGS.rpn_stride)
-                else:
-                    probs, params = sess.run([model.probs, model.params], feed_dict=feed_dict)
+                if is_val:
+                    # for validation set, produce the ground-truth boxes
+                    boxes_gt = sample.get_boxes_array(["Car"])
+                    if False:   # 2 lines below are for testing the C++ code
+                        probs_gt, _, params_gt, _ = model.vxl.voxelize_labels([boxes_gt], np.array(model.priors, dtype=np.float32), FLAGS.rpn_stride)
+                        boxes_bt = model.vxl.generate_boxes(probs_gt, params_gt, FLAGS.anchor_th)
+                    sample.load_boxes_array(boxes_gt, 'Car')
+                    # visualize groundtruth labels
+                    image3d = np.copy(sample.image2)
+                    for box in sample.label2:
+                        draw_box3d(image3d, box, sample.calib)
+                        pass
+                    cv2.imwrite(gal.next(), image3d)
+
+                probs, params = sess.run([model.probs, model.params], feed_dict=feed_dict)
+
                 boxes = model.vxl.generate_boxes(probs, params, FLAGS.anchor_th)
                 boxes = boxes[0]
                 print(np.max(probs), len(boxes))
@@ -54,7 +67,9 @@ def main (_):
                 for box in sample.label2:
                     draw_box3d(image3d, box, sample.calib)
                     pass
+
                 cv2.imwrite(gal.next(), image3d)
+
                 C += 1
                 if C >= FLAGS.max:
                     break
